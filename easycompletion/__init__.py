5b1d7acf0f81b11dc5b78b90752c0131755323b6
moon <shawmakesmagic@gmail.com>
2023-07-12 13:41:12 -0700

    Updated to 0.1.8 and published

--- easycompletion/__init__.py
+++ easycompletion/__init__.py
@@ -4,7 +4,7 @@ easycompletion
 Leveraging conversational AI for bicameral decision making.
 """
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __author__ = "Moon (https://github.com/lalalune)"
 __credits__ = "https://github.com/lalalune/easycompletion"
 
