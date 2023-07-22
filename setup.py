838690a1c1acfc26773fe3f7ca803a65b0e9547a
moon <shawmakesmagic@gmail.com>
2023-07-21 17:29:53 -0700

    0.2.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="easycompletion",
-    version='0.2.7',
+    version='0.2.8',
     description="Easy text completion and function calling using the OpenAI API. Also includes useful utilities for counting tokens, composing prompts and trimming them to fit within the token limit.",
     long_description=long_description,  # added this line
     long_description_content_type="text/markdown",  # and this line
